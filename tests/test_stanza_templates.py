e80cfea94ded968c03eab29b5771bcf97961e6ae
bleugreen <mitchell5799@gmail.com>
2026-07-01 17:27:27 -0700

    Sort stanza test imports

--- tests/test_stanza_templates.py
+++ tests/test_stanza_templates.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 
-from ksch.placed import PlacedJunction, PlacedLabel, PlacedSymbol, PlacedWire
-
 from test_block_layout import _can_controller_sheet
 
+from ksch.placed import PlacedJunction, PlacedLabel, PlacedSymbol, PlacedWire
 
 SENSE_GROUPS = (
     ("R10", "C7", "D6", "R12"),
